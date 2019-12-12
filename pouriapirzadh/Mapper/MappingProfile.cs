c389671be4d20dde9408eebad39ac5fd722a31d6
LENOVO <LENOVO@DESKTOP-BOS72FU>
2019-12-12 17:41:41 +0330

    r

--- pouriapirzadh/Mapper/MappingProfile.cs
+++ pouriapirzadh/Mapper/MappingProfile.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace Api.Mapper
 {
     public class MappingProfile:Profile
-    {
+    {//test
         public MappingProfile()
         {
             CreateMap<IdentityUser, SigninDto>()
